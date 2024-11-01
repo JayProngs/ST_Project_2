9467cb6e3094c0e4c34feeb91d9d164b364915e1
Jay Thakur <jsthakur@ncsu.edu>
2024-11-01 16:56:15 -0400

    added table output in data download tab.

--- SeoulBikeApp/app.R
+++ SeoulBikeApp/app.R
@@ -32,30 +32,30 @@ ui <- fluidPage(
     # Application title
     titlePanel("Seoul Bike Sharing Data Explorer"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar
     sidebarLayout(
         sidebarPanel(
           
-          # Widget to select first categorical variable for subsetting
+          # First categorical variable for subsetting
           selectInput("cat_var1", "Select Categorical Variable 1:",
                       choices = c("Seasons", "Holiday", "Functioning_Day"),
                       selected = "Seasons"),
           uiOutput("cat_var1_levels"),
           
-          # Widget to select second categorical variable for subsetting
+          # Second categorical variable for subsetting
           selectInput("cat_var2", "Select Categorical Variable 2:",
                       choices = c("Seasons", "Holiday", "Functioning_Day"),
                       selected = "Holiday"),
           uiOutput("cat_var2_levels"),
           
-          # Widget to select first numeric variable for subsetting
+          # First numeric variable for subsetting
           selectInput("num_var1", "Select Numeric Variable 1:",
                       choices = c("Temperature", "Humidity", "Wind_speed",
                                   "Visibility", "Solar_Radiation", "Rainfall", "Snowfall"),
                       selected = "Temperature"),
           uiOutput("num_var1_slider"),
           
-          # Widget to select second numeric variable for subsetting
+          # Second numeric variable for subsetting
           selectInput("num_var2", "Select Numeric Variable 2:",
                       choices = c("Temperature", "Humidity", "Wind_speed",
                                   "Visibility", "Solar_Radiation", "Rainfall", "Snowfall"),
@@ -65,12 +65,11 @@ ui <- fluidPage(
           # Action button to subset data
           actionButton("update_button", "Update Data"),
           
-          # Instructions
           helpText("Select variables and click 'Update Data' to apply subsetting.")
           
         ),
 
-        # Show a plot of the generated distribution
+
         mainPanel(
           tabsetPanel(
             tabPanel("About", 
@@ -100,20 +99,72 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+# Define server logic
+server <- function(input, output, session) {
+  
+  # Reactive values to store the subsetted data
+  values <- reactiveValues(data = df)
+  
+  # First categorical variable
+  output$cat_var1_levels <- renderUI({
+    req(input$cat_var1)
+    selectInput("cat_var1_level", paste("Select Levels of", input$cat_var1),
+                choices = levels(as.factor(df[[input$cat_var1]])),
+                selected = levels(as.factor(df[[input$cat_var1]])),
+                multiple = TRUE)
+  })
+  
+  # Second categorical variable
+  output$cat_var2_levels <- renderUI({
+    req(input$cat_var2)
+    selectInput("cat_var2_level", paste("Select Levels of", input$cat_var2),
+                choices = levels(as.factor(df[[input$cat_var2]])),
+                selected = levels(as.factor(df[[input$cat_var2]])),
+                multiple = TRUE)
+  })
+  
+  # First numeric variable
+  output$num_var1_slider <- renderUI({
+    req(input$num_var1)
+    numeric_var <- df[[input$num_var1]]
+    sliderInput("num_var1_range", paste("Select Range of", input$num_var1),
+                min = min(numeric_var, na.rm = TRUE),
+                max = max(numeric_var, na.rm = TRUE),
+                value = c(min(numeric_var, na.rm = TRUE), max(numeric_var, na.rm = TRUE)))
+  })
+  
+  # Second numeric variable
+  output$num_var2_slider <- renderUI({
+    req(input$num_var2)
+    numeric_var <- df[[input$num_var2]]
+    sliderInput("num_var2_range", paste("Select Range of", input$num_var2),
+                min = min(numeric_var, na.rm = TRUE),
+                max = max(numeric_var, na.rm = TRUE),
+                value = c(min(numeric_var, na.rm = TRUE), max(numeric_var, na.rm = TRUE)))
+  })
+  
+  # Subset the data
+  observeEvent(input$update_button, {
+    subset_data <- df %>%
+      filter(
+        .data[[input$cat_var1]] %in% input$cat_var1_level,
+        .data[[input$cat_var2]] %in% input$cat_var2_level,
+        .data[[input$num_var1]] >= input$num_var1_range[1],
+        .data[[input$num_var1]] <= input$num_var1_range[2],
+        .data[[input$num_var2]] >= input$num_var2_range[1],
+        .data[[input$num_var2]] <= input$num_var2_range[2]
+      )
+    values$data <- subset_data
+  })
+  
+  # show the data in the Data Download tab
+  output$data_table <- DT::renderDataTable({
+    DT::datatable(values$data)
+  })
+  
 }
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
